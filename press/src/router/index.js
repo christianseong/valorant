1d2c1f17924d249c05a81b00ecec7265c9858562
parkjunhoo <adobephotoshop@naver.com>
2021-06-26 14:20:43 +0900

    init

--- press/src/router/index.js
+++ press/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import axios from 'axios'
+import axios from 'axios'
 
 Vue.use(VueRouter)
 
@@ -51,184 +51,184 @@ const routes = [
     path: '/admin',
     name: 'Admin',
     component: () => import('../views/Admin.vue'),
-    // beforeEnter: (to,from,next) => {
-    //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-    //   .then((res)=>{
-    //     console.log(res.data);
-    //     if(res.data==='not_logged'||res.data==='not_admin')
-    //     {
-    //       router.push('/login');
-    //       return;
-    //     }
-    //     else{
-    //       if(res.data.info.admin)
-    //       {
-    //         // router.push('/admin/article');
-    //         next();
-    //         return;
-    //       }
-    //     }
-    //   });
-    // },
+    beforeEnter: (to,from,next) => {
+      axios.get('http://alldayfootball.co.kr/api/auth/check')
+      .then((res)=>{
+        console.log(res.data);
+        if(res.data==='not_logged'||res.data==='not_admin')
+        {
+          router.push('/login');
+          return;
+        }
+        else{
+          if(res.data.info.admin)
+          {
+            // router.push('/admin/article');
+            next();
+            return;
+          }
+        }
+      });
+    },
     children: [
       {
         path : '/admin/write',
         name : 'Write',
         component: () => import('../components/Admin/Write.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.admin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.admin)
+              {
+                next();
+                return;
+              }
+            }
+          });
+        },
       },
       {
         path : '/admin/edit',
         name : 'Edit',
         component: () => import('../components/Admin/Edit.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.admin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.admin)
+              {
+                next();
+                return;
+              }
+            }
+          });
+        },
       },
       {
         path : '/admin/article',
         name : 'Article',
         component: () => import('../components/Admin/Article.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.admin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.admin)
+              {
+                next();
+                return;
+              }
+            }
+          });
+        },
       },
       {
         path : '/admin/ad',
         name : 'Ad',
         component: () => import('../components/Admin/Ad.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.superAdmin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //       else{
-        //         alert('권한이 없습니다.');
-        //         router.push('/admin/article');
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.superAdmin)
+              {
+                next();
+                return;
+              }
+              else{
+                alert('권한이 없습니다.');
+                router.push('/admin/article');
+                return;
+              }
+            }
+          });
+        },
       },
       {
         path : '/admin/accout',
         name : 'Accout',
         component: () => import('../components/Admin/Accout.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.superAdmin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //       else{
-        //         alert('권한이 없습니다.');
-        //         router.push('/admin/article');
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.superAdmin)
+              {
+                next();
+                return;
+              }
+              else{
+                alert('권한이 없습니다.');
+                router.push('/admin/article');
+                return;
+              }
+            }
+          });
+        },
       },
       {
         path : '/admin/configtext',
         name : 'Configtext',
         component: () => import('../components/Admin/ConfigText.vue'),
-        // beforeEnter: (to,from,next) => {
-        //   axios.get('http://alldayfootball.co.kr/api/auth/check')
-        //   .then((res)=>{
-        //     // console.log(res.data);
-        //     if(res.data==='not_logged'||res.data==='not_admin')
-        //     {
-        //       alert('권한이 없습니다.');
-        //       router.push('/admin/login');
-        //       return;
-        //     }
-        //     else{
-        //       if(res.data.info.superAdmin)
-        //       {
-        //         next();
-        //         return;
-        //       }
-        //       else{
-        //         alert('권한이 없습니다.');
-        //         router.push('/admin/article');
-        //         return;
-        //       }
-        //     }
-        //   });
-        // },
+        beforeEnter: (to,from,next) => {
+          axios.get('http://alldayfootball.co.kr/api/auth/check')
+          .then((res)=>{
+            // console.log(res.data);
+            if(res.data==='not_logged'||res.data==='not_admin')
+            {
+              alert('권한이 없습니다.');
+              router.push('/admin/login');
+              return;
+            }
+            else{
+              if(res.data.info.superAdmin)
+              {
+                next();
+                return;
+              }
+              else{
+                alert('권한이 없습니다.');
+                router.push('/admin/article');
+                return;
+              }
+            }
+          });
+        },
       },
     ],
   },
